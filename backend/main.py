d41f7ad9040db151feb04eaea16517da2b0392f4
fenix473 <favi.libero@gmail.com>
2026-03-11 18:08:35 -0500

    Added localhost work

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://trader-module-frontend.vercel.app"],
+    allow_origins=["https://trader-module-frontend.vercel.app", "http://localhost:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
